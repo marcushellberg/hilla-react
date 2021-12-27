dcf1ea78a785965d78259775516f0159b2ebf2e6
Marcus Hellberg <marcus@vaadin.com>
2021-12-27 15:47:43 -0800

    base react + hilla app

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,17 +1,4 @@
-import { Router } from '@vaadin/router';
-import { routes } from './routes';
-import { appStore } from './stores/app-store';
+import './main';
 
-export const router = new Router(document.querySelector('#outlet'));
-
-router.setRoutes(routes);
-
-window.addEventListener('vaadin-router-location-changed', (e) => {
-  appStore.setLocation((e as CustomEvent).detail.location);
-  const title = appStore.currentViewTitle;
-  if (title) {
-    document.title = title + ' | ' + appStore.applicationName;
-  } else {
-    document.title = appStore.applicationName;
-  }
-});
+// This file is for Hilla interop.
+// Hilla expects an index.ts file as the entrypoint.
--- src/main/java/dev/hilla/demo/Application.java
+++ src/main/java/dev/hilla/demo/Application.java
@@ -1,8 +1,6 @@
 package dev.hilla.demo;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,13 +8,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 /**
  * The entry point of the Spring Boot application.
  *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
+ * Use the @PWA annotation make the application installable on phones, tablets and some desktop
+ * browsers.
  *
  */
 @SpringBootApplication
-@Theme(value = "lakka")
-@PWA(name = "Lakka", shortName = "Lakka", offlineResources = {"images/logo.png"})
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
