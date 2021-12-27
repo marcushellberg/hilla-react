5b4b0dc5638cc53afe3b48db7feeacabc6ac1424
start.vaadin.com <no-reply@vaadin.com>
2021-12-27 23:17:26 +0000

    Generated project

---
+++ frontend/routes.ts
@@ -0,0 +1,13 @@
+import { Route } from '@vaadin/router';
+import './views/main-layout';
+
+export type ViewRoute = Route & {
+  title?: string;
+  icon?: string;
+  children?: ViewRoute[];
+};
+
+export const views: ViewRoute[] = [
+  // place routes below (more info https://vaadin.com/docs/latest/fusion/routing/overview)
+];
+export const routes: ViewRoute[] = [...views];
